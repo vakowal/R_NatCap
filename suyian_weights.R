cc93f44904bbe39a88ed6c98cff47a0c7592d83e
Ginger Kowal <ginger.kowal@gmail.com>
2016-09-20 17:42:00 -0400

    added plot of simulated data compared to empirical

--- suyian_weights.R
+++ suyian_weights.R
@@ -31,7 +31,8 @@ png(file=pngname, units="in", res=300, width=7, height=5)
 print(p)
 dev.off()
 
-w_only <- data[, c(5:16)]
+# first cohort only
+w_only <- data[which(data$animal_id < 100), c(5:9, 11, 13)]
 w_means <- colMeans(w_only, na.rm=TRUE)
 w_min <- sapply(w_only, min, na.rm=TRUE)
 w_max <- sapply(w_only, max, na.rm=TRUE)
@@ -41,7 +42,7 @@ q75 <- sapply(w_only, quantile, probs=c(0.75),
               na.rm=TRUE, names=FALSE)
 w_mean_df <- data.frame('id'=rep(0, length(w_means)),
                         'kg'=w_means,
-                        'date'=dates,
+                        'date'=colnames(data)[c(5:9, 11, 13)],
                         'min'=w_min,
                         'max'=w_max,
                         'q25'=q25,
@@ -53,7 +54,29 @@ p <- p + geom_line()
 p <- p + geom_ribbon(aes(ymin=q25, ymax=q75), alpha=0.2)
 print(p)
 
-pngname <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Data/Kenya/Suyian_ranch_steer_weights/steer_mean_25_75_perc.png"
+pngname <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Data/Kenya/Suyian_ranch_steer_weights/steer_cohort1_mean_25_75_perc.png"
+png(file=pngname, units="in", res=300, width=7, height=5)
+print(p)
+dev.off()
+
+#### simulation results to replicate these weights
+sim_df <- read.csv("C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/Verification_calculations/Suyian_cattle_weights/run_summary.csv")
+sim_df$date <- as.Date(paste('30', sim_df$month, sim_df$year, sep="-"),
+                       format='%d-%m-%Y')
+sim_df <- sim_df[, c('date', 'run', 'cattle_kg')]
+sim_df$run <- as.factor(sim_df$run)
+
+join_df <- w_mean_df[, c('kg', 'date')]
+colnames(join_df) <- c('cattle_kg', 'date')
+join_df$run <- rep('empirical mean', dim(join_df)[1])
+
+fig_df <- rbind(sim_df, join_df)
+
+p <- ggplot(fig_df, aes(x=date, y=cattle_kg, group=run))
+p <- p + geom_line(aes(colour=run))
+print(p)
+
+pngname <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/Verification_calculations/Suyian_cattle_weights/run_comparison.png"
 png(file=pngname, units="in", res=300, width=7, height=5)
 print(p)
 dev.off()
