df0f99008c9ee2e1ea541d19cdee93f684d042cd
Ginger Kowal <ginger.kowal@gmail.com>
2016-10-18 14:17:05 -0400

    new script to analyze net economic return data from household surveys

---
+++ CGIAR_net_gain.R
@@ -0,0 +1,81 @@
+## processing net gain data from Peru household survey
+library(ggplot2)
+
+gain_csv <- "C:/Users/Ginger/Dropbox/NatCap_backup/CGIAR/Peru/Household_survey_livestock_portion/Net_gain_10.4.16.csv"
+weight_csv <- "C:/Users/Ginger/Dropbox/NatCap_backup/CGIAR/Peru/Household_survey_livestock_portion/Animal_weight_10.5.16.csv"
+
+gain_df <- read.csv(gain_csv, skip=1)
+weight_df <- read.csv(weight_csv)
+weight_means <- colMeans(weight_df, na.rm=TRUE)
+
+combined <- merge(gain_df, weight_df, by='id_hogar')
+sheepm <- lm(ganancia_neta_ovinos~Sheep..kg.per.animal.sold., combined)
+summary(sheepm)
+
+cheesem <- lm(queso_ganancia_unitaria~Cows..kg.per.animal.sold., combined)
+summary(cheesem)
+sheepwoolm <- lm(lana_ganancia_unitaria~Sheep..kg.per.animal.sold., combined)
+summary(sheepwoolm)
+alpacawoolm <- lm(fibra_alpaca_ganancia_unitaria~Alpacas..kg.per.animal.sold., combined)
+summary(alpacawoolm)
+
+sheeptotallm <- lm(ganancia_neta_ovinos_procesados~Sheep..kg.per.animal.sold., combined)
+cattletotallm <- lm(ganancia_neta_vacunos_procesados~Cows..kg.per.animal.sold., combined)
+alpacatotallm <- lm(ganancia_neta_alpacas_procesados~Alpacas..kg.per.animal.sold., combined)
+summary(sheeptotallm)
+summary(cattletotallm)
+summary(alpacatotallm)
+
+p <- ggplot(combined, aes(x=Cows..kg.per.animal.sold., y=queso_ganancia_unitaria))
+p <- p + geom_point()
+p <- p + geom_abline(intercept=-213.179, slope=1.6753)
+print(p)
+pngname <- paste(imgdir, "Cow_cheese.png", sep="/")
+png(file=pngname, units="in", res=300, width=5, height=4)
+print(p)
+dev.off()
+
+bulldf <- combined[, c('Bulls..kg.per.animal.sold.', 'ganancia_neta_vacunos')]
+cowdf <- combined[, c('Cows..kg.per.animal.sold.', 'ganancia_neta_vacunos')]
+colnames(bulldf)[1] <- 'cattle_weight_kg'
+colnames(cowdf)[1] <- 'cattle_weight_kg'
+cattledf <- rbind(bulldf, cowdf)
+cattlem <- lm(ganancia_neta_vacunos~cattle_weight_kg, cattledf)
+summary(cattlem)
+
+alpacadf <- combined[, c('Alpacas..kg.per.animal.sold.', 'ganancia_neta_alpacas')]
+llamadf <- combined[, c('Llamas..kg.per.animal.sold.', 'ganancia_neta_llamas')]
+colnames(alpacadf) <- c('camelid_weight_kg', 'ganancia_neta')
+colnames(llamadf) <- c('camelid_weight_kg', 'ganancia_neta')
+cameliddf <- rbind(alpacadf, llamadf)
+camelidm <- lm(ganancia_neta~camelid_weight_kg, cameliddf)
+summary(camelidm)
+
+imgdir <- "C:/Users/Ginger/Dropbox/NatCap_backup/CGIAR/Peru/Household_survey_livestock_portion/net_gain_figs"
+
+p <- ggplot(combined, aes(x=Sheep..kg.per.animal.sold., y=ganancia_neta_ovinos))
+p <- p + geom_point()
+p <- p + geom_abline(intercept=98.19, slope=1.13)
+print(p)
+pngname <- paste(imgdir, "Sheep.png", sep="/")
+png(file=pngname, units="in", res=300, width=5, height=4)
+print(p)
+dev.off()
+
+p <- ggplot(cattledf, aes(x=cattle_weight_kg, y=ganancia_neta_vacunos))
+p <- p + geom_point()
+p <- p + geom_abline(intercept=736.5832, slope=0.8924)
+print(p)
+pngname <- paste(imgdir, "Cattle.png", sep="/")
+png(file=pngname, units="in", res=300, width=5, height=4)
+print(p)
+dev.off()
+
+p <- ggplot(cameliddf, aes(x=camelid_weight_kg, y=ganancia_neta))
+p <- p + geom_point()
+p <- p + geom_abline(intercept=172.7764, slope=0.6386)
+print(p)
+pngname <- paste(imgdir, "Camelids.png", sep="/")
+png(file=pngname, units="in", res=300, width=5, height=4)
+print(p)
+dev.off()
