84f91913a668e7a31cc36a1f286ff1edefb5a4b1
Ginger Kowal <ginger.kowal@gmail.com>
2016-12-27 17:47:04 -0500

    new functions to analyze dung density around weather stations on OPC

--- dung_summary.R
+++ dung_summary.R
@@ -1,7 +1,10 @@
 # summarize dung data for Kenya Ticks
 library(ggplot2)
 
-# summarize OPC dung data for calculating correlation with GPS-derived stocking density
+count <- function(values){
+  return(length(values))
+}
+
 metadata_csv <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Data/Kenya/From_Sharon/Processed_by_Ginger/OPC_veg_data_9.30.16_metadata.csv"
 PDM_csv <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Data/Kenya/From_Sharon/Processed_by_Ginger/OPC_veg_data_9.30.16_PDM.csv"
 
@@ -13,8 +16,79 @@ dung_df[, 9:32][is.na(dung_df[, 9:32])] <- 0
 dung_ids <- unique(dung_df[, c("Date", "Site")])
 
 dung_df$transect <- paste(dung_df$Date, dung_df$Site, sep="_")
+# make sure there are 11 samples in each transect
+sample_count <- aggregate(dung_df$PDM, by=list(dung_df$transect), FUN=count)
+trouble <- sample_count[which(sample_count$PDM != 11), ]  # should be 0 row
+
 dung_sum <- aggregate(dung_df[, 9:32], by=list(dung_df$transect), FUN=sum)
 colnames(dung_sum)[1] <- 'transect'
+
+meta_df$transect <- paste(meta_df$Date, meta_df$Site, sep="_")
+meta_subs <- meta_df[, c('transect', 'Lat', 'Long')]
+date_only <- dung_df[, c('transect', 'Date')]
+date_only <- date_only[!duplicated(date_only[, 'transect']), ]
+
+comb_df <- merge(dung_sum, meta_subs, by='transect')
+comb_df <- merge(comb_df, date_only, by='transect')
+save_as <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Data/Kenya/OPC_dung_analysis/dung_summary.csv"
+write.csv(comb_df, file=save_as, row.names=TRUE)
+
+# dung around weather stations summarized in python 
+outerdir <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/Kenya_ticks_project_specific/OPC_dung_analysis/OPC_weather_stations"
+points_file <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Data/Kenya/Climate/OPC_weather_stations_coordinates.csv"
+group_key <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/Kenya_ticks_project_specific/wildlife_group_definition.csv"
+gr_key_df <- read.csv(group_key)
+
+points_df <- read.csv(points_file)
+df_list <- list()
+for(stn in points_df$Name){
+  dung_file <- paste(outerdir, paste("dung_2.0km_", stn, ".csv", sep=""), sep="/")
+  df <- read.csv(dung_file)
+  df_sub <- df[which(df$Year == 2015), c(3:26)]
+  df_means <- as.data.frame.list(colMeans(df_sub))
+  df_means$site <- stn
+  df_means$n_transect <- dim(df_sub)[1]
+  df_list[[stn]] <- df_means
+}
+combined <- do.call(rbind, df_list)
+
+means_t <- as.data.frame(t(combined[, c(1:24)]))
+means_t$Abbrev <- rownames(means_t)
+
+gr_subs <- gr_key_df[, c('Abbrev', 'Group1', 'Group2')]
+comb <- merge(means_t, gr_subs, by='Abbrev')
+
+gr1_means <- aggregate(comb[, 2:11], by=list(comb$Group1), FUN=mean)
+gr2_means <- aggregate(comb[, 2:11], by=list(comb$Group2), FUN=mean)
+colnames(gr1_means)[1] <- "group"
+colnames(gr2_means)[1] <- "group"
+
+# reshape for plotting
+gr1_res <- reshape(gr1_means, idvar="group", varying=list(2:11), timevar='site',
+                   v.names="mean_dung", times=colnames(gr1_means)[2:11], direction="long",
+                   new.row.names=1:1000)
+gr2_res <- reshape(gr2_means, idvar="group", varying=list(2:11), timevar='site',
+                   v.names="mean_dung", times=colnames(gr1_means)[2:11], direction="long",
+                   new.row.names=1:1000)
+
+gr1_plot <- gr1_res[which(gr1_res$group == 'non-cattle'), ]
+gr1_plot <- gr1_plot[which(gr1_plot$site %in% c('Kamok', 'Loidien',
+                                                'Loirugurugu', 'Research',
+                                                'Rongai gate', ' Serat gate')), ]
+p <- ggplot(gr1_plot, aes(x=site, y=mean_dung))
+p <- p + geom_point()
+print(p)
+
+gr2_plot <- gr2_res[which(gr2_res$group != 'cattle'), ]
+gr2_plot <- gr2_plot[which(gr2_plot$site %in% c('Kamok', 'Loidien',
+                                                'Loirugurugu', 'Research',
+                                                'Rongai gate', ' Serat gate')), ]
+p <- ggplot(gr2_plot, aes(x=site, y=mean_dung, group=group))
+p <- p + geom_point(aes(colour=group))
+print(p)
+
+
+# summarize OPC dung data for calculating correlation with GPS-derived stocking density
 dung_subs <- dung_sum[, c('transect', 'Buf', 'Cow')]
 
 meta_df$transect <- paste(meta_df$Date, meta_df$Site, sep="_")
