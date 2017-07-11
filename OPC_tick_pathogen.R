13aed1786c844c4a9b473651638b85cb29d9d3c8
Ginger <ginger.kowal@gmail.com>
2017-07-11 14:13:59 -0400

    link pathogen data transects with original transects file (sample_ids)

--- OPC_tick_pathogen.R
+++ OPC_tick_pathogen.R
@@ -14,17 +14,8 @@ for(r in 1:NROW(pathogen_df)){
 }
 pathogen_df <- pathogen_df[pathogen_df$Site == 'Ol Pejeta  ' |
                              pathogen_df$Site == 'Ol Pejeta' |
-                             pathogen_df$Site == 'OPC extra', ]
+                             pathogen_df$Site == 'Ol Pejeta_extra', ]
 pathogen_df$transect <- paste(pathogen_df$Date, pathogen_df$Code, sep="_")
-
-
-ticks_meta_df <- read.csv("C:/Users/Ginger/Box Sync/Kenya Fame and Fortune Starts Here Data Portal/Ol_Pej/Project files_Ol Pejeta/OPC_tick_metadata.csv")
-veg_meta_df <- read.csv("C:/Users/Ginger/Box Sync/Kenya Fame and Fortune Starts Here Data Portal/Ol_Pej/Project files_Ol Pejeta/OPC_veg_data_9.30.16_metadata.csv")
-tick_transects <- read.csv("C:/Users/Ginger/Box Sync/Kenya Fame and Fortune Starts Here Data Portal/Ol_Pej/Project files_Ol Pejeta/OPC_tick_transects.csv")
-ticks_meta_df$site_transect <- paste(ticks_meta_df$Date, ticks_meta_df$Site, sep="_")
-ticks_meta_df$way_transect <- paste(ticks_meta_df$Date, ticks_meta_df$waypoint, sep="_")
-tick_transects$transect <- paste(tick_transects$Date, tick_transects$Site,
-                                 sep="_")
 
 # try to deduce how to match pathogen sites with metadata sites
 in_a_not_b <- function(a_ids, b_ids){
@@ -38,17 +29,56 @@ in_a_not_b <- function(a_ids, b_ids){
 }
 
 pathogen_ids <- unique(pathogen_df$transect)
-meta_way_ids <- ticks_meta_df$way_transect
-meta_site_ids <- ticks_meta_df$site_transect
-transect_ids <- tick_transects$transect
 
-miss_1 <- in_a_not_b(pathogen_ids, meta_way_ids)
-miss_2 <- in_a_not_b(pathogen_ids, meta_site_ids)
-miss_3 <- in_a_not_b(pathogen_ids, transect_ids)
-miss_4 <- in_a_not_b(transect_ids, meta_way_ids)
-miss_5 <- in_a_not_b(transect_ids, meta_site_ids)
-miss_6 <- in_a_not_b(meta_site_ids, transect_ids)
-miss_7 <- in_a_not_b(meta_way_ids, transect_ids)
+work_dir <- "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Data/Kenya/From_Brian/pathogen_data_metadata_mismatch"
+sample_ids <- read.csv(paste(work_dir, "KWS_samples for export_finalv2.csv", sep="/"))
+sample_ids$transect <- paste(sample_ids$Date, sample_ids$Code, sep="_")
+write.csv(sample_ids, paste(work_dir, "sample_ids_with_transect.csv", sep="/"),
+          row.names=FALSE)
 
+sample_ids <- sample_ids[sample_ids$Site == 'Ol Pejeta  ' |
+                           sample_ids$Site == 'Ol Pejeta' |
+                           sample_ids$Site == 'Ol Pejeta_extra', ]
+sample_ids <- sample_ids[!is.na(sample_ids$Code), ]
+sample_id_transects <- sample_ids$transect
+missing_from_meta <- in_a_not_b(pathogen_ids, sample_id_transects)
+missing_from_path <- in_a_not_b(sample_id_transects, pathogen_ids)
 
-# reconcile tick transect metadata with veg transect metadata corrected??
+meta_orig <- read.csv(paste(work_dir, "opc_metadata_orig.csv", sep="/"))
+meta_orig$transect <- paste(meta_orig$Date, meta_orig$waypoint, sep="_")
+
+miss_1 <- in_a_not_b(pathogen_ids, meta_orig$transect)
+
+pathogen_df$missing_from_meta_orig_waypoint <- 0
+for(r in 1:NROW(pathogen_df)){
+  if(pathogen_df[r, 'transect'] %in% miss_1){
+    pathogen_df[r, 'missing_from_meta_orig_waypoint'] <- 1
+  }
+}
+pathogen_df <- pathogen_df[, c(4:6, 44:45)]
+pathogen_df <- pathogen_df[!duplicated(pathogen_df$transect), ]
+
+missing_codes <- in_a_not_b(unique(pathogen_df$Code), meta_orig$waypoint)
+write.csv(missing_codes, paste(work_dir, "missing_waypoints.csv", sep="/"),
+          row.names=FALSE)
+
+# find missing records not due to missing codes
+missing_transects <- data.frame('missing_from_metadata'=miss_1)
+missing_transects$missing_due_to_code <- 0
+missing_transects$missing_due_to_date <- 0
+for(r in 1:NROW(missing_transects)){
+  code <- strsplit(as.character(missing_transects[r, 'missing_from_metadata']),
+                   split="_")[[1]][2]
+  if(code %in% missing_codes){
+    missing_transects[r, 'missing_due_to_code'] <- 1
+  }
+  else{
+    missing_transects[r, 'missing_due_to_date'] <- 1
+  }
+}
+pathogen_df <- merge(pathogen_df, missing_transects, by.x='transect',
+                     by.y='missing_from_metadata', all=TRUE)
+pathogen_df[is.na(pathogen_df$missing_due_to_code), 'missing_due_to_code'] <- 0
+pathogen_df[is.na(pathogen_df$missing_due_to_date), 'missing_due_to_date'] <- 0
+write.csv(pathogen_df, paste(work_dir, "pathogen_with_missing_records.csv", sep="/"),
+          row.names=FALSE)
