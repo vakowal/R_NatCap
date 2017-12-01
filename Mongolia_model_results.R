4967b1f7fbe58c36a256f7867d717ccaec35d948
Ginger Kowal <ginger.kowal@gmail.com>
2017-12-01 15:12:26 -0500

    process model results to maps

--- Mongolia_model_results.R
+++ Mongolia_model_results.R
@@ -2,17 +2,52 @@
 
 library(ggplot2)
 
+match_df <- read.csv("C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/data/climate/NAMEM/soum_ctr_match_table.csv")
+
+# make map of CHIRPS pixel-based model results
+ch_df <- read.csv("C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/model_results/CHIRPS_pixels/biomass_summary_zero_sd_chirps_GCD_G.csv")
+aug_df <- ch_df[ch_df$month == 9, colnames(ch_df)[c(2:5, 9)]]
+aug_wide <- reshape(aug_df, idvar='site_id', timevar='year', direction="wide")
+write.csv(aug_wide,
+          "C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/model_results/CHIRPS_pixels/biomass_August_2016_2017_2_soums.csv",
+          row.names=FALSE)
+
 # compare results from NAMEM and CHIRPS
 comp_df <- read.csv("C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/model_results/soum_centers/biomass_summary_zero_sd_namem_chirps_GCD_G.csv")
 comp_df <- comp_df[comp_df$month == 9, ]  # August only
-p <- ggplot(comp_df, aes(x=site_id, y=total_biomass_gm2))
+match_df <- match_df[, c('name_en', 'site_id')]
+comp_df <- merge(comp_df, match_df)
+p <- ggplot(comp_df, aes(x=name_en, y=total_biomass_gm2))
 p <- p + geom_point(aes(shape=climate_source))
 p <- p + facet_wrap(~year)
+p <- p + xlab("Soum") + ylab("August biomass (g/m2)")
 print(p)
-pngname <- "C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/model_results/soum_centers/figs/August_biomass_2016_2017.png"
-png(file=pngname, units="in", res=300, width=6, height=4)
+pngname <- "C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/model_results/soum_centers/figs/soum_ctr_August_biomass_2016_2017.png"
+png(file=pngname, units="in", res=300, width=8, height=4)
 print(p)
 dev.off()
+
+# make csv to join to soum centers, to make map
+comp_df <- read.csv("C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/model_results/soum_centers/biomass_summary_zero_sd_namem_chirps_GCD_G.csv")
+comp_subs <- comp_df[comp_df$month == 9, colnames(comp_df)[c(2:5, 7, 9)]]  # August only
+comp_nm <- comp_df[comp_df$month == 9 &
+                       comp_df$climate_source == 'namem_clim',
+                     colnames(comp_df)[c(2:5, 9)]]
+comp_wide <- reshape(comp_nm, idvar='site_id',
+                     timevar='year',
+                     direction="wide")
+write.csv(comp_wide,
+          "C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/model_results/soum_centers/biomass_August_2016_2017_NAMEM.csv",
+          row.names=FALSE)
+comp_ch <- comp_df[comp_df$month == 9 &
+                     comp_df$climate_source == 'chirps_prec',
+                   colnames(comp_df)[c(2:5, 9)]]
+comp_wide <- reshape(comp_ch, idvar='site_id',
+                     timevar='year',
+                     direction="wide")
+write.csv(comp_wide,
+          "C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/model_results/soum_centers/biomass_August_2016_2017_CHIRPS.csv",
+          row.names=FALSE)
 
 # compare NAMEM and CHIRPS climate data at soum centers
 match_df <- read.csv("C:/Users/Ginger/Dropbox/NatCap_backup/Mongolia/data/climate/NAMEM/soum_ctr_match_table.csv")
@@ -50,17 +85,30 @@ png(file=pngname, units="in", res=300, width=15, height=10)
 print(p)
 dev.off()
 
+clim_sub <- clim_res[clim_res$variable == "prec  ", ]
+p <- ggplot(clim_sub, aes(x=date, y=amount))
+p <- p + geom_line(aes(linetype=source))
+p <- p + facet_wrap(~site_id, scales="free",
+                    nrow=3)
+print(p)
+pngname <- paste(figdir, "CHIRPS_vs_NAMEM_prec.png", sep="/")
+png(file=pngname, units="in", res=300, width=5, height=8)
+print(p)
+dev.off()
+
 ch_subs <- clim_res[clim_res$source == 'CHIRPS', ]
 nm_subs <- clim_res[clim_res$source == 'NAMEM', ]
 sub_res <- merge(ch_subs, nm_subs, by=c('date', "Year", 'month', 'variable', 'site_id'))
 sub_res$CHIRPS_minus_NAMEM <- sub_res$amount.x - sub_res$amount.y
+sub_prec <- sub_res[sub_res$variable == unique(sub_res$variable)[1], ]
 
-p <- ggplot(sub_res, aes(x=date, y=CHIRPS_minus_NAMEM))
+p <- ggplot(sub_prec, aes(x=date, y=CHIRPS_minus_NAMEM))
 p <- p + geom_line()
-p <- p + facet_grid(variable~site_id, scales="free")
+p <- p + facet_wrap(~site_id, nrow=1)  #, scales="free")
+p <- p + ylab("CHIRPS - NAMEM (cm)")
 print(p)
-pngname <- paste(figdir, "CHIRPS_minus_NAMEM.png", sep="/")
-png(file=pngname, units="in", res=300, width=15, height=10)
+pngname <- paste(figdir, "CHIRPS_minus_NAMEM_prec.png", sep="/")
+png(file=pngname, units="in", res=300, width=8, height=5)
 print(p)
 dev.off()
 
