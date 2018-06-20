91a08d701754218d95d22aec1bc03e2c84a8d940
Ginger <ginger.kowal@gmail.com>
2018-06-19 20:40:45 -0400

    collect Century output variables

--- Century_documentation.R
+++ Century_documentation.R
@@ -13,7 +13,7 @@ pdef <- read.csv(param_def_csv, stringsAsFactors=FALSE)
 cols <- colnames(pdef)[4:15]
 pdef$definition <- do.call(paste, c(pdef[cols], sep=" "))
 pdef$definition <- trimws(pdef$definition, which="right")
-# concatenate consequetive comments
+# concatenate consecutive comments
 for(r in seq(nrow(pdef), 1, by=-1)){
   if(nchar(pdef[r, 'parameter']) == 0){
     pdef[r-1, 'definition'] <- paste(pdef[r-1, 'definition'],
@@ -84,9 +84,56 @@ p_table$char_of <- NA
 p_table[p_table$file == 'fix.100', 'char_of'] <- 'site'
 p_table[p_table$file == 'site.100', 'char_of'] <- 'site'
 p_table[p_table$file == 'crop.100', 'char_of'] <- 'PFT'
+p_table[p_table$file == 'graz.100', 'char_of'] <- 'PFT'
 p_table[p_table$rp_name == 'nlaypg', 'char_of'] <- 'PFT'
 colnames(p_table) <- c("Century parameter name", "Definition", "Valid values",
                        "Example value", "Example value source", "Century input file",
                        "Rangeland production model name", "Property of")
 write.csv(p_table, "C:/Users/ginge/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/GK_doc/Century_parameter_table.csv",
+          row.names=FALSE)
+
+##### output variables
+# from users manual
+users_manual_vars_raw <- read.csv("C:/Users/ginge/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/GK_doc/Century_output_var_users_manual.csv",
+                                  header=FALSE, stringsAsFactors=FALSE)
+users_manual_vars <- data.frame('outvar'=users_manual_vars_raw[, 1],
+                                stringsAsFactors=FALSE)
+cols <- colnames(users_manual_vars_raw)[2:length(colnames(users_manual_vars_raw))]
+users_manual_vars$definition <- do.call(paste,
+                                        c(users_manual_vars_raw[cols], sep=" "))
+users_manual_vars$definition <- trimws(users_manual_vars$definition, which="right")
+users_manual_vars <- users_manual_vars[users_manual_vars$outvar != '', ]
+users_manual_vars <- users_manual_vars[users_manual_vars$outvar != 'Err:509', ]
+users_manual_vars$unix <- NA
+users_manual_vars[grep("UNIX ONLY", users_manual_vars$definition), 'unix'] <- 1
+pc_vars <- users_manual_vars[is.na(users_manual_vars$unix),
+                             c('outvar', 'definition')]
+
+plot1_vars_raw <- read.csv("C:/Users/ginge/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/GK_doc/Century_output_var_plot1.csv",
+                           header=FALSE, stringsAsFactors=FALSE)
+plot1_vars <- c()
+for(r in 1:NROW(plot1_vars_raw)){
+  plot1_vars <- c(plot1_vars,
+                  unlist(strsplit(plot1_vars_raw[r, ], split=",")))
+}
+
+plot1_vars <- data.frame('outvar'=plot1_vars)
+vars_combined <- merge(plot1_vars, pc_vars, all=TRUE)
+vars_combined$definition <- trimws(vars_combined$definition, which="both")
+common_to_both <- intersect(plot1_vars$outvar, pc_vars$outvar)
+vars_combined[vars_combined$outvar %in% common_to_both, 'source'] <- 'both'
+vars_combined[is.na(vars_combined$definition)
+              & is.na(vars_combined$source), 'source'] <- 'plot1'
+vars_combined[is.na(vars_combined$source), 'source'] <- 'users_manual'
+vars_combined <- vars_combined[order(vars_combined$source, vars_combined$outvar), ]
+
+# identify output variables that are state variables
+state_vars_df <- read.csv("C:/Users/ginge/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/GK_doc/Century_state_variables.csv")
+state_vars <- sapply(state_vars_df$State_variable_Century, tolower)
+vars_combined[vars_combined$outvar %in% state_vars, 'derivation'] <- 'state_variable'
+
+vars_combined[grep("^annual accumulator", vars_combined$definition), 'derivation'] <- 'annual accumulator'
+
+write.csv(vars_combined,
+          "C:/Users/ginge/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/GK_doc/Century_output_variables.csv",
           row.names=FALSE)
